4219738deb630f4dfe303183ef5842b4a4f0d1be
Haifeng Mei <haifengmei@hotmail.com>
2018-11-25 21:41:49 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker build -t [Your Docker Hub Username]/go-ceries-cloud_app
+sudo docker build -t [Your Docker Hub Username]/go-ceries-cloud_app .
 sudo docker push [Your Docker Hub Username]/go-ceries-cloud_app
 ecs-cli configure --region us-east-1 --cluster goceriesTest
 ecs-cli up --keypair [Your KeyPair File Name] --capability-iam --size 2 --instance-type t2.micro
