78c4b0db1b3fde406f397f093b8ad9721bee0d25
Haifeng Mei <haifengmei@hotmail.com>
2018-11-25 21:29:18 -0400

    added command script

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo docker build -t [Your Docker Hub Username]/go-ceries-cloud_app
+sudo docker push [Your Docker Hub Username]/go-ceries-cloud_app
+ecs-cli configure --region us-east-1 --cluster goceriesTest
+ecs-cli up --keypair [Your KeyPair File Name] --capability-iam --size 2 --instance-type t2.micro
+ecs-cli compose --file aws-compose.yml up
+ecs-cli ps
