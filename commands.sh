9b1e06e0a65e84b43a2c66bc500559e3ce25cfdf
Haifeng Mei <haifengmei@hotmail.com>
2018-11-25 23:25:14 -0400

    updated script to loop a time related command

--- commands.sh
+++ commands.sh
@@ -3,5 +3,5 @@ sudo docker build -t [Your Docker Hub Username]/go-ceries-cloud_app .
 sudo docker push [Your Docker Hub Username]/go-ceries-cloud_app
 ecs-cli configure --region us-east-1 --cluster goceriesTest
 ecs-cli up --keypair [Your KeyPair File Name] --capability-iam --size 2 --instance-type t2.micro
-ecs-cli compose --file aws-compose.yml up
+for i in 1 2 3 4 5; do ecs-cli compose --file aws-compose.yml up && break || sleep 15; done
 ecs-cli ps
